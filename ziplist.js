fcf6321b1652f603f35a710574892ee26b1ac4e5
susankpma <susanm@hawaii.edu>
2021-02-08 19:20:23 -1000

    Update ziplist.js

--- ziplist.js
+++ ziplist.js
@@ -1,4 +1,4 @@
-function zipList (list1, list2) {
+function zipList(list1, list2) {
   const newList = [];
 
   for (let i = 0; i < list1.length; i++) {
@@ -10,7 +10,7 @@ function zipList (list1, list2) {
 
 console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
 
-function zipListTheSimpleWay (list1, list2) {
+function zipListTheSimpleWay(list1, list2) {
   return _.flatten(_.zip(list1, list2));
 }
 console.log(zipListTheSimpleWay(['a', 'b', 'c'], [1, 2, 3]));
